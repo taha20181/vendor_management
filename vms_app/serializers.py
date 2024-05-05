423dc8bd42380eda80da55b4ea46d2ee790dec7d
taha20181 <tahamustafa053@gmail.com>
2024-05-05 21:27:25 +0530

    delivery date as read only

--- vms_app/serializers.py
+++ vms_app/serializers.py
@@ -16,6 +16,7 @@ class VendorPerformanceSerializer(serializers.ModelSerializer):
 
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     acknowledgment_date = serializers.DateTimeField(read_only=True)
+    delivery_date = serializers.DateTimeField(read_only=True)
     class Meta:
         model = PurchaseOrder
         fields = ['id', 'po_number', 'vendor', 'order_date', 'expected_delivery_date', 'delivery_date', 'items', 'quantity', 'status', 'quality_rating', 'issue_date', 'acknowledgment_date']
